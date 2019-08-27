c47e709b56d2e9df249000d0abff0c32ed956a11
Yari Andres <yari.andres@gmail.com>
2019-08-27 17:41:33 +0200

    first commit

---
+++ introToCsharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace introToCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            // int number;
+            // float floatingNum;
+            double PI;
+            PI = 3.14;
+            Console.WriteLine(PI);
+
+            double storingPI;
+            storingPI = PI;
+
+            decimal deci;
+            deci = System.Convert.ToDecimal(PI);
+            Console.WriteLine(deci);
+
+            string[] animals = new string[3];
+            animals[0] = "deer";
+            animals[1] = "moose";
+            animals[2] = "boars";
+            Console.WriteLine("ARRAY 1: " + animals.Length);
+
+
+        }
+
+        
+    }
+
+    
+}
+ 
